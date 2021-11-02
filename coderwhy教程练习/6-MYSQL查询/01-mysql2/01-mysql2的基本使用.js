bf98a1d6f48190aa819542bf5e4e940d5bc5cb35
yanyinuo <1601530253@qq.com>
2021-11-02 22:15:54 +0800

    21-11-2

---
+++ coderwhy教程练习/6-MYSQL查询/01-mysql2/01-mysql2的基本使用.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'yyn990902',
+    database: 'codehub'
+})
+
+const statement = `
+SELECT stu.id stuId, stu.name stuName, stu.age stuAge, 
+JSON_ARRAYAGG(JSON_OBJECT('id',cs.id,'name',cs.name,'price',cs.price)) msg
+FROM students stu
+JOIN student_select_course ssc ON stu.id = ssc.student_id
+JOIN course cs ON ssc.course_id = cs.id
+GROUP BY stuId;
+`
+
+connection.query(statement, (err, results, fields) => {
+    console.log(results);
+    console.log(fields);
+})
